93c12c2a1ba11fda1de04eef2c7b5ddf00b82089
Spencer James <spencer@gitwit.com>
2017-06-06 11:06:33 -0500

    debugging

--- hil_button_gtm_communicator.js
+++ hil_button_gtm_communicator.js
@@ -2,8 +2,10 @@
 // register a form submission
 try {
 // 	   parent.postMessage('formSubmit', 'https://hiltibutton.us.hilti.com/');
+	   console.log('Pre-message');
  	   parent.postMessage('formSubmit', 'http://hil-button.herokuapp.com'); 	   
 // 	   parent.postMessage('formSubmit', 'https://form.hilti.com'); 	   
+	   console.log('Post-message');
 } catch(e) {
   	// Something went wrong...
   	window.console && window.console.log(e);

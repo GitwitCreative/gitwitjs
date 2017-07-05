d88886ea08a468dbb45ab770c1d9b60e4e059fcb
Spencer James <spencer@gitwit.com>
2017-07-05 14:44:20 -0500

    minor

--- hil_button_gtm_communicator.js
+++ hil_button_gtm_communicator.js
@@ -1,11 +1,11 @@
 // Communicate with the parent site hiltibutton.us.hilti.com to
 // register a form submission
 try {
-// 	   parent.postMessage('formSubmit', 'https://hiltibutton.us.hilti.com/');
-	   console.log('Pre-message');
- 	   parent.postMessage('formSubmit', 'http://hil-button.herokuapp.com'); 	   
+ 	   parent.postMessage('formSubmit', 'https://hiltibutton.us.hilti.com/');
+//	   console.log('Pre-message');
+// 	   parent.postMessage('formSubmit', 'http://hil-button.herokuapp.com'); 	   
 // 	   parent.postMessage('formSubmit', 'https://form.hilti.com'); 	   
-	   console.log('Post-message');
+//	   console.log('Post-message');
 } catch(e) {
   	// Something went wrong...
   	window.console && window.console.log(e);

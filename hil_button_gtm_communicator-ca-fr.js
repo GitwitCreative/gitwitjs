01e54ba0138983833a2ddbfde40eb9f28d633739
gitwitjordan <jordanwinn@gmail.com>
2017-08-05 01:18:22 -0500

    Add files via upload
    Additional Hilti Button form scripts for CA

---
+++ hil_button_gtm_communicator-ca-fr.js
@@ -0,0 +1,12 @@
+// Communicate with the parent site hiltibutton.us.hilti.com to
+// register a form submission
+try {
+ 	   parent.postMessage('formSubmit', 'https://hiltibutton.hilti.ca/fr/');
+//	   console.log('Pre-message');
+// 	   parent.postMessage('formSubmit', 'http://hil-button.herokuapp.com'); 	   
+// 	   parent.postMessage('formSubmit', 'https://form.hilti.com'); 	   
+//	   console.log('Post-message');
+} catch(e) {
+  	// Something went wrong...
+  	window.console && window.console.log(e);
+}
